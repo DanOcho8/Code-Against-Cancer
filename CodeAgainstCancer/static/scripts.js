9f56ed365949fd27b7200504725941b1c3519fb1
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-09-21 17:37:02 -0700

    fix sidebar not closing

--- CodeAgainstCancer/static/scripts.js
+++ CodeAgainstCancer/static/scripts.js
@@ -4,15 +4,10 @@ function toggleSidebar() {
   if (mini) {
     console.log("opening sidebar");
     document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    this.mini = false;
+    mini = false;
   } else {
     console.log("closing sidebar");
     document.getElementById("mySidebar").style.width = "50px";
-    document.getElementById("main").style.marginLeft = "50px";
-    this.mini = true;
+    mini = true;
   }
 }
-
-
-
