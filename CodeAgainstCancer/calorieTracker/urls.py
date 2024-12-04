f86dd2ad05cae89a257b55fbb89b9059908b475f
ecardenas31 <ecardenas31@outlook.com>
2024-12-04 14:22:26 -0800

    editing the add_calorie_entry in views.py and changing the set up in calorie.html

--- CodeAgainstCancer/calorieTracker/urls.py
+++ CodeAgainstCancer/calorieTracker/urls.py
@@ -5,7 +5,8 @@ app_name = "calorieTracker"
 
 urlpatterns = [
     path('calorie/', views.calorie_tracker, name='calorie_tracker'),  # Main calorie tracker page
-    path('add/', views.add_calorie_entry, name='add_calorie_entry'),  # Add a new calorie entry
+    path('add/', views.add_calorie_entry, name='add_entry'),  # Add a new calorie entry
     path('delete/<int:entry_id>/', views.delete_calorie_entry, name='delete_calorie_entry'),  # Delete a specific entry
     path('edit/<int:entry_id>/', views.edit_calorie_entry, name='edit_calorie_entry'),  # Edit a specific entry
+    
 ]
