57288b5d90f0ce7c88f67a769334609929b28b4a
David Vu <DejaVu@Davids-MacBook-Pro.local>
2024-12-06 21:20:54 -0800

    Merge branch 'david' - Added donation button, redesigned donation page, added dynamic contributors wall, contributors wall has a manual moderation verification system, added total contributions display

--- CodeAgainstCancer/accounts/models.py
+++ CodeAgainstCancer/accounts/models.py
@@ -11,6 +11,19 @@ class UserProfile(models.Model):
     profile_pic = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
     phone_number = PhoneNumberField(null=True, blank=True)
     consent_to_text = models.BooleanField(default=False)
+
     def __str__(self):
         return self.user.username
+
+class Donor(models.Model):
+    email = models.EmailField(max_length=254, blank=True, null=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    name = models.CharField(max_length=50, blank=True, null=True, default="Anonymous")
+    message = models.TextField(blank=True, null=True, max_length=150)
+    date = models.DateTimeField(auto_now_add=True)
+    validated = models.BooleanField(default=False)
     
+    def __str__(self):
+        verification_status = "Verified" if self.validated else "Not Verified"
+        amount_display = f"${self.amount}" if self.amount else "No Amount"
+        return f"{self.name} ({self.email}) - {amount_display} - {verification_status}"
